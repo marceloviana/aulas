34c52261461af95f1bf1d07aa5b00abf2045f902
Marcelo Almeida <marcelo.almeida@marcelo.almeida-V7G1>
2024-04-29 07:25:54 -0300

    autla 26-04

--- introducao_programacao/assets/js/funcoes.js
+++ introducao_programacao/assets/js/funcoes.js
@@ -1,8 +1,11 @@
 var itensCarrinho = []
 
 function adicionarItemCarrinho (id) {
-    itensCarrinho.push(produtos[id])
-
+    
+    if (id) {
+        itensCarrinho.push(produtos[id])
+    }
+    
     // Configure total itens:
     document.getElementById("contador").innerText = itensCarrinho.length
 
@@ -11,15 +14,11 @@ function adicionarItemCarrinho (id) {
     const initialValue = 0;
     document.getElementById("dropdownItens").innerHTML = `Total: R$ ${itensCarrinho.reduce((accumulator, currentValue) => accumulator + ~~currentValue.preco, initialValue)}`;
     for (let item of itensCarrinho) {
-        document.getElementById("dropdownItens").innerHTML += `<li>${item.nome} - R$ ${item.preco}</li>`
+        document.getElementById("dropdownItens").innerHTML += `<li>${item.nome} - R$ ${item.preco} <button onclick=removeItenCarrinho(${item.id - 1})>remover</button></li>`
     }    
 }
 
-function removeItenCarrinho (indice) {
-    document.getElementById("dropdownItens").innerHTML = ""
-
-    itensCarrinho.splice(indice, 1)
-    for (let item of itensCarrinho) {
-        document.getElementById("dropdownItens").innerHTML += `<li>${item.nome} - R$ ${item.preco}</li>`
-    }    
+function removeItenCarrinho (id) {
+    itensCarrinho.splice(id, 1)
+    adicionarItemCarrinho(null)
 }
